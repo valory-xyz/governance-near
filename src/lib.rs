8d98ca571bd549cbdb136a2872d3495cf8971b06
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2024-11-18 20:32:28 +0000

    refactor: moving code around

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ use near_sdk::store::iterable_set::IterableSet;
 use near_sdk::{
     env, near, ext_contract, require, AccountId, Promise, PromiseOrValue, Gas, PromiseResult, NearToken, log, serde_json
 };
-use sha2::{Sha256, Digest};
 
 pub mod byte_utils;
 pub mod state;
@@ -17,33 +16,49 @@ pub trait Wormhole {
 
 // Prepaid gas for a single (not inclusive of recursion) `verify_vaa` call.
 const COMPLETE_CALL_GAS_NUM: u64 = 5;
-const VERIFY_CALL_GAS_NUM: u64 = 20;
 const VERIFY_CALL_GAS: Gas = Gas::from_tgas(VERIFY_CALL_GAS_NUM);
+// Prepaid gas for VAA verification
+const VERIFY_CALL_GAS_NUM: u64 = 20;
 const COMPLETE_CALL_GAS: Gas = Gas::from_tgas(COMPLETE_CALL_GAS_NUM);
+// Maximum number of calls
 const MAX_NUM_CALLS: usize = 10;
 
+// Call struct: target, value, gas, selector, arguments
 #[derive(BorshDeserialize, Serialize, Deserialize)]
 pub struct Call {
+    // Target contract
     pub contract_id: AccountId,
+    // Deposit value amount
     pub deposit: NearToken,
-    pub gas: u64, // max 300 tgas
+    // Function call gas
+    pub gas: u64,
+    // Selector name
     pub method_name: String,
+    // Arguments as vector of bytes
     pub args: Vec<u8>,
 }
 
+// Call result: success and result as vector of bytes
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CallResult {
     pub success: bool,
     pub result: Option<Vec<u8>>,
 }
 
+// WormholeMessenger contract:
 #[near(contract_state)]
 pub struct WormholeMessenger {
+    // Contract owner account Id
     owner: AccountId,
+    // Wormhole Core account Id
     wormhole_core: AccountId,
-    foreign_governor_address: Vec<u8>,
-    chain_id: u16,
+    // Foreign governor emitter in bytes form
+    foreign_governor_emitter: Vec<u8>,
+    // Foreign chain Id
+    foreign_chain_id: u16,
+    // Processed VAA hashes
     dups: IterableSet<Vec<u8>>,
+    // Contract upgrade hash
     upgrade_hash: Vec<u8>,
 }
 
@@ -52,8 +67,8 @@ impl Default for WormholeMessenger {
         Self {
             owner: "".parse().unwrap(),
             wormhole_core: "".parse().unwrap(),
-            foreign_governor_address: Vec::new(),
-            chain_id: 0,
+            foreign_governor_emitter: Vec::new(),
+            foreign_chain_id: 0,
             dups: IterableSet::new(b"d".to_vec()),
             upgrade_hash: Vec::new()
         }
@@ -66,103 +81,36 @@ impl WormholeMessenger {
     pub fn new(
         owner_id: AccountId,
         wormhole_core: AccountId,
-        foreign_governor_address: Vec<u8>,
-        chain_id: u16
+        foreign_governor_emitter: Vec<u8>,
+        foreign_chain_id: u16
     ) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         Self {
             owner: owner_id,
             wormhole_core,
-            foreign_governor_address,
-            chain_id,
+            foreign_governor_emitter,
+            foreign_chain_id,
             dups: IterableSet::new(b"d".to_vec()),
             upgrade_hash: b"h".to_vec()
         }
     }
 
-    pub fn version(&self) -> String {
-        env!("CARGO_PKG_VERSION").to_owned()
-    }
+    #[private]
+    pub fn refund_deposit_to_account(&self, storage_used: u64, deposit: NearToken, account_id: AccountId, deposit_in: bool) {
+        let mut required_cost = env::storage_byte_cost().saturating_mul(storage_used.into());
+        required_cost = required_cost.saturating_add(deposit);
 
-    pub fn get_storage_usage(&self) -> u64 {
-        env::storage_usage()
-    }
-
-    pub fn get_foreign_governor_address(&self) -> Vec<u8> {
-        self.foreign_governor_address.clone()
-    }
-
-    pub fn to_bytes(&self, calls: Vec<Call>) -> Vec<u8> {
-        serde_json::to_vec(&calls).expect("Failed to serialize Vec<Call>")
-    }
-
-	pub fn update_contract_work(&self) {
-        // Receive the code directly from the input to avoid the
-        // GAS overhead of deserializing parameters
-        let code = env::input().expect("Error: No input").to_vec();
-
-        let hash = env::sha256(&code);
-
-        // Check the caller is authorized to update the code
-        //if hash != self.upgrade_hash {
-        //    env::panic_str("invalidUpgradeContract");
-        //}
-
-        env::log_str(&format!(
-            "WormholeMessenger/{}#{}: : {}",
-            file!(),
-            line!(),
-            hex::encode(&hash)
-        ));
-
-        // Deploy the contract on self
-        Promise::new(env::current_account_id())
-            .deploy_contract(code);
-    }
-
-    #[payable]
-    pub fn delivery(&mut self, vaa: String) -> Promise {
-        let initial_storage_usage = env::storage_usage();
-
-        let calls = self.process_vaa(vaa.clone());
-
-        let storage = env::storage_usage() - initial_storage_usage;
-
-        // TODO Set a limit for calls.len
-        require!(calls.len() <= MAX_NUM_CALLS, "Exceeded max number of calls");
-
-        let mut sum_deposit = NearToken::from_yoctonear(0);
-        let mut sum_gas = 0;
-        for call in calls.iter() {
-            sum_deposit = sum_deposit.saturating_add(call.deposit);
-            sum_gas = sum_gas + call.gas + COMPLETE_CALL_GAS_NUM;
-            log!("call contract_id: {}", call.contract_id);
-            log!("call deposit: {}", call.deposit);
-            log!("call method_name: {}", call.method_name);
-            log!("call args: {:?}", call.args);
-            log!("call gas: {:?}", call.gas);
+        let mut refund = env::attached_deposit().into();
+        if deposit_in {
+            require!(required_cost <= refund, "Insufficient required cost");
+            refund = refund.saturating_sub(required_cost);
+        } else {
+            require!(required_cost <= env::account_balance(), "Insufficient required cost");
+            refund = refund.saturating_add(required_cost);
         }
-        sum_gas = sum_gas + VERIFY_CALL_GAS_NUM + COMPLETE_CALL_GAS_NUM;
-        require!(env::prepaid_gas() > Gas::from_tgas(sum_gas), "Exceeded max gas");
-
-
-        self.refund_deposit_to_account(storage, sum_deposit, env::predecessor_account_id(), true);
-
-        let promise = Promise::new(self.wormhole_core.clone())
-            .function_call(
-                "verify_vaa".to_string(),
-                serde_json::json!({ "vaa": vaa }).to_string().as_bytes().to_vec(),
-                NearToken::from_yoctonear(0),
-                VERIFY_CALL_GAS
-            );
-
-        // Pass all the calls and 0-th index of a promise
-        promise.then(
-            Self::ext(env::current_account_id())
-                .with_static_gas(Gas::from_tgas(sum_gas))
-                .with_attached_deposit(sum_deposit)
-                .on_complete(calls, 0),
-        )
+        if refund > NearToken::from_yoctonear(1) {
+            Promise::new(account_id).transfer(refund);
+        }
     }
 
     #[private]
@@ -180,7 +128,7 @@ impl WormholeMessenger {
         self.dups.insert(parsed_vaa.hash);
         self.dups.flush();
 
-        if parsed_vaa.emitter_chain != self.chain_id || parsed_vaa.emitter_address != self.foreign_governor_address {
+        if parsed_vaa.emitter_chain != self.foreign_chain_id || parsed_vaa.emitter_address != self.foreign_governor_emitter {
             env::panic_str("InvalidGovernorEmitter");
         }
 
@@ -224,29 +172,97 @@ impl WormholeMessenger {
         // Check account validity
         require!(env::is_valid_account_id(&new_foreign_governor_address), "Account Id is invalid");
 
-        self.foreign_governor_address = new_foreign_governor_address;
+        self.foreign_governor_emitter = new_foreign_governor_address.clone();
 
-        // TODO: event
+        env::log_str(&format!(
+            "WormholeMessenger/{}#{}: : {}",
+            file!(),
+            line!(),
+            hex::encode(&new_foreign_governor_address)
+        ));
     }
 
-    #[private]
-    pub fn refund_deposit_to_account(&self, storage_used: u64, deposit: NearToken, account_id: AccountId, deposit_in: bool) {
-        let mut required_cost = env::storage_byte_cost().saturating_mul(storage_used.into());
-        required_cost = required_cost.saturating_add(deposit);
+	pub fn update_contract(&self) {
+        // Receive the code directly from the input to avoid the
+        // GAS overhead of deserializing parameters
+        let code = env::input().expect("Error: No input").to_vec();
 
-        let mut refund = env::attached_deposit().into();
-        if deposit_in {
-            require!(required_cost <= refund, "Insufficient required cost");
-            refund = refund.saturating_sub(required_cost);
-        } else {
-            require!(required_cost <= env::account_balance(), "Insufficient required cost");
-            refund = refund.saturating_add(required_cost);
+        let hash = env::sha256(&code);
+
+        // Check if caller is authorized to update the contract code
+        if hash != self.upgrade_hash {
+           env::panic_str("InvalidUpgradeContractHash");
         }
-        if refund > NearToken::from_yoctonear(1) {
-            Promise::new(account_id).transfer(refund);
+
+        env::log_str(&format!(
+            "WormholeMessenger/{}#{}: : {}",
+            file!(),
+            line!(),
+            hex::encode(&hash)
+        ));
+
+        // Deploy the contract on self
+        Promise::new(env::current_account_id())
+            .deploy_contract(code);
+    }
+
+    #[payable]
+    pub fn delivery(&mut self, vaa: String) -> Promise {
+        let initial_storage_usage = env::storage_usage();
+
+        let calls = self.process_vaa(vaa.clone());
+
+        let storage = env::storage_usage() - initial_storage_usage;
+
+        require!(calls.len() <= MAX_NUM_CALLS, "Exceeded max number of calls");
+
+        let mut sum_deposit = NearToken::from_yoctonear(0);
+        let mut sum_gas = 0;
+        for call in calls.iter() {
+            sum_deposit = sum_deposit.saturating_add(call.deposit);
+            sum_gas = sum_gas + call.gas + COMPLETE_CALL_GAS_NUM;
+            log!("call contract_id: {}", call.contract_id);
+            log!("call deposit: {}", call.deposit);
+            log!("call method_name: {}", call.method_name);
+            log!("call args: {:?}", call.args);
+            log!("call gas: {:?}", call.gas);
         }
+        sum_gas = sum_gas + VERIFY_CALL_GAS_NUM + COMPLETE_CALL_GAS_NUM;
+        require!(env::prepaid_gas() > Gas::from_tgas(sum_gas), "Exceeded max gas");
+
+
+        self.refund_deposit_to_account(storage, sum_deposit, env::predecessor_account_id(), true);
+
+        let promise = Promise::new(self.wormhole_core.clone())
+            .function_call(
+                "verify_vaa".to_string(),
+                serde_json::json!({ "vaa": vaa }).to_string().as_bytes().to_vec(),
+                NearToken::from_yoctonear(0),
+                VERIFY_CALL_GAS
+            );
+
+        // Pass all the calls and 0-th index of a promise
+        promise.then(
+            Self::ext(env::current_account_id())
+                .with_static_gas(Gas::from_tgas(sum_gas))
+                .with_attached_deposit(sum_deposit)
+                .on_complete(calls, 0),
+        )
+    }
+
+    pub fn get_foreign_governor_emitter(&self) -> Vec<u8> {
+        self.foreign_governor_emitter.clone()
+    }
+
+    pub fn get_foreign_chain_id(&self) -> u16 {
+        self.foreign_chain_id
+    }
+
+    pub fn version(&self) -> String {
+        env!("CARGO_PKG_VERSION").to_owned()
+    }
+
+    pub fn get_storage_usage(&self) -> u64 {
+        env::storage_usage()
     }
 }
-
-
-
